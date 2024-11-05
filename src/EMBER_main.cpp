0ca7c8dd9ae3493776bdf698e76f2c4889a67876
Sung Woo Lee <raylee9919@gmail.com>
2024-11-06 00:35:46 +0900

    ...

--- src/EMBER_main.cpp
+++ src/EMBER_main.cpp
@@ -15,9 +15,13 @@
 
 enum Token_Type
 {
+    INVALID = -1,
     END_OF_FILE,
 
-    OPEN_PAREN, CLOSE_PAREN, OPEN_BRACE, CLOSE_BRACE, OPEN_BRACKET, CLOSE_BRACKET,
+    LPAREN, RPAREN,
+    LBRACE, RBRACE,
+    LBRACKET,
+    RBRACKET,
     PLUS, PLUS_PLUS, PLUS_EQUAL,
     MINUS, MINUS_MINUS, MINUS_EQUAL,
     STAR, STAR_EQUAL,
@@ -55,19 +59,74 @@ struct Lexer
     char *end;
 };
 
-static b32
+inline b32
+is_whitespace(char c)
+{
+    b32 result = (c == ' ' || c == '\t' || c == '\r' || c == '\n');
+    return result;
+}
+
+inline b32
 is_next_line(char c)
 {
     b32 result = (c == '\r' || c == '\n');
     return result;
 }
 
-#define push_token(LEXER, TYPE) _push_token(LEXER, TYPE, #TYPE)
+#define case_is_alphabet \
+    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z': \
+    case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z'
+
+inline b32
+is_alphabet_or_underscore(char c)
+{
+    b32 result = ( (c >= 'a' && c <= 'z') ||
+                   (c >= 'A' && c <= 'Z') ||
+                   (c == '_'));
+    return result;
+}
+
+inline b32
+is_digit(char c)
+{
+    b32 result = (c >= '0' && c<= '9');
+    return result;
+}
+
+#define push_token_and_advance(LEXER, TYPE) _push_token_and_advance(LEXER, TYPE, #TYPE)
 static void
-_push_token(Lexer *lexer, Token_Type type, const char *type_literal)
+_push_token_and_advance(Lexer *lexer, Token_Type type, const char *type_literal)
 {
     size_t literal_len = (lexer->hi - lexer->lo);
     printf("%.*s (%s)\n", (s32)literal_len, lexer->lo, type_literal);
+    lexer->lo = lexer->hi++;
+}
+
+inline char
+peek_safe(Lexer *lexer)
+{
+    char result;
+
+    if (lexer->hi < lexer->end)
+        result = *lexer->hi;
+    else
+        result = 0;
+
+    return result;
+}
+
+inline void
+advance(Lexer *lexer)
+{
+    lexer->lo = lexer->hi++;
+}
+
+inline char
+peek(Lexer *lexer)
+{
+    char result;
+    result = *lexer->hi;
+    return result;
 }
 
 static void
@@ -87,112 +146,85 @@ tokenize(Buffer code)
         {
             case '\0': // EOF
             {
-                push_token(&lx, END_OF_FILE);
+                push_token_and_advance(&lx, END_OF_FILE);
                 should_continue = 0;
             } break;
 
-            case '(':
-            {
-                push_token(&lx, OPEN_PAREN);
-                lx.lo = lx.hi++;
-            } break;
-            case ')':
-            {
-                push_token(&lx, CLOSE_PAREN);
-                lx.lo = lx.hi++;
-            } break;
-            case '[':
-            {
-                push_token(&lx, OPEN_BRACKET);
-                lx.lo = lx.hi++;
-            } break;
-            case ']':
-            {
-                push_token(&lx, CLOSE_BRACKET);
-                lx.lo = lx.hi++;
-            } break;
-            case '{':
-            {
-                push_token(&lx, OPEN_BRACE);
-                lx.lo = lx.hi++;
-            } break;
-            case '}':
-            {
-                push_token(&lx, CLOSE_BRACE);
-                lx.lo = lx.hi++;
-            } break;
+            case '(': push_token_and_advance(&lx, LPAREN); break;
+            case ')': push_token_and_advance(&lx, RPAREN); break;
+            case '[': push_token_and_advance(&lx, LBRACKET); break;
+            case ']': push_token_and_advance(&lx, RBRACKET); break;
+            case '{': push_token_and_advance(&lx, LBRACE); break;
+            case '}': push_token_and_advance(&lx, RBRACE); break;
 
             case '+':
             {
-                if (lx.hi < lx.end)
+                switch (peek_safe(&lx))
                 {
-                    if (*lx.hi == '+')
+                    case '+':
                     {
                         ++lx.hi;
-                        push_token(&lx, PLUS_PLUS);
-                        lx.lo = lx.hi++;
-                    }
-                    else if (*lx.hi == '=')
+                        push_token_and_advance(&lx, PLUS_PLUS);
+                    } break;
+
+                    case '=':
                     {
                         ++lx.hi;
-                        push_token(&lx, PLUS_EQUAL);
-                        lx.lo = lx.hi++;
-                    }
-                    else
+                        push_token_and_advance(&lx, PLUS_EQUAL);
+                    } break;
+
+                    default:
                     {
-                        push_token(&lx, PLUS);
-                        lx.lo = lx.hi++;
-                    }
+                        push_token_and_advance(&lx, PLUS);
+                    } break;
                 }
             } break;
 
             case '-':
             {
-                if (lx.hi < lx.end)
+                switch (peek_safe(&lx))
                 {
-                    if (*lx.hi == '-')
+                    case '-':
                     {
                         ++lx.hi;
-                        push_token(&lx, MINUS_MINUS);
-                        lx.lo = lx.hi++;
-                    }
-                    else if (*lx.hi == '=')
+                        push_token_and_advance(&lx, MINUS_MINUS);
+                    } break;
+
+                    case '=':
                     {
                         ++lx.hi;
-                        push_token(&lx, MINUS_EQUAL);
-                        lx.lo = lx.hi++;
-                    }
-                    else
+                        push_token_and_advance(&lx, MINUS_EQUAL);
+                    } break;
+
+                    default:
                     {
-                        push_token(&lx, MINUS);
-                        lx.lo = lx.hi++;
-                    }
+                        push_token_and_advance(&lx, MINUS);
+                    } break;
                 }
             } break;
 
             case '*':
             {
-                if (lx.hi < lx.end)
+                switch (peek_safe(&lx))
                 {
-                    if (*lx.hi == '=')
+                    case '=':
                     {
                         ++lx.hi;
-                        push_token(&lx, STAR_EQUAL);
-                        lx.lo = lx.hi++;
-                    }
-                    else
+                        push_token_and_advance(&lx, STAR_EQUAL);
+                    } break;
+
+                    default:
                     {
-                        push_token(&lx, STAR);
-                        lx.lo = lx.hi++;
-                    }
+                        push_token_and_advance(&lx, STAR);
+                    } break;
                 }
             } break;
 
             case '/':
             {
-                if (lx.hi < lx.end)
+                switch (peek_safe(&lx))
                 {
-                    if (*lx.hi == '/') // Single-line comment
+                    case '/':
                     {
                         ++lx.hi;
                         while (lx.hi < lx.end &&
@@ -202,11 +234,13 @@ tokenize(Buffer code)
                         }
 
 #if _INTERNAL
-                        push_token(&lx, DEBUG_SINGLE_LINE_COMMENT);
-#endif
+                        push_token_and_advance(&lx, DEBUG_SINGLE_LINE_COMMENT);
+#else
                         lx.lo = lx.hi++;
-                    }
-                    else if (*lx.hi == '*') // Multi-line comment
+#endif
+                    } break;
+
+                    case '*':
                     {
                         ++lx.hi;
                         for (;;)
@@ -219,31 +253,46 @@ tokenize(Buffer code)
                                 {
                                     ++lx.hi;
 #if _INTERNAL
-                                    push_token(&lx, DEBUG_MULTI_LINE_COMMENT);
-#endif
+                                    push_token_and_advance(&lx, DEBUG_MULTI_LINE_COMMENT);
+#else
                                     lx.lo = lx.hi++;
+#endif
                                     break;
                                 }
                             }
                         }
-                    }
-                    else if (*lx.hi == '=')
+                    } break;
+
+                    case '=':
                     {
                         ++lx.hi;
-                        push_token(&lx, SLASH_EQUAL);
-                        lx.lo = lx.hi++;
-                    }
-                    else
+                        push_token_and_advance(&lx, SLASH_EQUAL);
+                    } break;
+
+                    default:
                     {
-                        push_token(&lx, SLASH);
-                        lx.lo = lx.hi++;
-                    }
+                        push_token_and_advance(&lx, SLASH);
+                    } break;
                 }
             } break;
 
             default:
             {
-                lx.lo = lx.hi++;
+                if (is_whitespace(c)) // @NOTE: if you want the info of current line, separate this.
+                {
+                    lx.lo = lx.hi++;
+                }
+                else if (is_alphabet_or_underscore(c))
+                {
+                }
+                else if (is_digit(c))
+                {
+                }
+                else
+                {
+                    // @TODO: ERROR
+                    lx.lo = lx.hi++;
+                }
             } break;
         }
     }
@@ -251,14 +300,15 @@ tokenize(Buffer code)
 
 int main(void)
 {
-    Buffer code = read_entire_file_and_null_terminate("main.ember");
+    const char *file_name = "main.ember";
+    Buffer code = read_entire_file_and_null_terminate(file_name);
     if (code.size)
     {
         tokenize(code);
     }
     else
     {
-        fprintf(stderr, "ERROR\n");
+        fprintf(stderr, "ERROR: Couldn't read file %s.\n", file_name);
     }
 
     return 0;
