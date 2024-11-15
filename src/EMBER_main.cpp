435c6233a14b477b11163e0a8a7a9270977ff1b0
Sung Woo Lee <raylee9919@gmail.com>
2024-11-15 23:05:47 +0900

    Pratt Parsing

--- src/EMBER_main.cpp
+++ src/EMBER_main.cpp
@@ -20,8 +20,7 @@
 
 #define DEBUG_min_prec -1219
 
-struct Parser
-{
+struct Parser {
     Parser(Token_List *token_list) {
         this->token_list = token_list;
         this->next = token_list->base;
@@ -32,6 +31,8 @@ struct Parser
 };
 
 enum Ast_Node_Type : u8 {
+    AST_STATEMENT,
+
     AST_S32,
 
     AST_BINARY_PLUS,
@@ -41,6 +42,7 @@ enum Ast_Node_Type : u8 {
     AST_BINARY_GREATER,
     AST_BINARY_LESS,
 };
+
 struct Ast_Node {
     Ast_Node_Type type;
     s32 val;
@@ -65,18 +67,21 @@ is_binary_operator(Token token) {
 
 static s32
 get_precedence(Token token) {
+    s32 result;
     switch (token.type) {
         case Token_Type::LESS:
-        case Token_Type::GREATER:{ return 1; } break;
+        case Token_Type::GREATER:{ result = 1; } break;
 
         case Token_Type::PLUS:
-        case Token_Type::MINUS:{ return 2; } break;
+        case Token_Type::MINUS:{ result = 2; } break;
 
         case Token_Type::STAR:
-        case Token_Type::SLASH: { return 3; } break;
+        case Token_Type::SLASH: { result = 3; } break;
 
         INVALID_DEFAULT_CASE;
     }
+
+    return result;
 }
 
 static Ast_Node *parse_expression(Parser &parser, s32 min_prec);
@@ -84,13 +89,13 @@ static Ast_Node *parse_expression(Parser &parser, s32 min_prec);
 static Ast_Node *
 make_binary(Ast_Node *left, Ast_Node *binary, Ast_Node *right) {
     binary->left = left;
-    binary->right = left;
+    binary->right = right;
     return binary;
 }
 
 static Ast_Node *
 to_operator(Token token) {
-    Ast_Node *node = new Ast_Node;
+    Ast_Node *node = new Ast_Node{};
     switch (token.type) {
         case Token_Type::PLUS: {
             node->type = AST_BINARY_PLUS;
@@ -123,28 +128,8 @@ to_operator(Token token) {
 }
 
 static Ast_Node *
-parse_increasing_precedence(Parser &parser, Ast_Node *left, s32 min_prec) {
-    Ast_Node *result;
-
-    Token next = *parser.next++;
-
-    if (is_binary_operator(next)) {
-        s32 next_prec = get_precedence(next);
-
-        if (next_prec <= min_prec) {
-            result = left;
-        } else {
-            Ast_Node *right = parse_expression(parser, next_prec);
-            result = make_binary(left, to_operator(next), right);
-        }
-    }
-
-    return result;
-}
-
-static Ast_Node *
 parse_leaf(Parser &parser) {
-    Ast_Node *node = new Ast_Node; // @TODO: mem management
+    Ast_Node *node = new Ast_Node{}; // @TODO: mem management
 
     Token tk = *parser.next++;
     if (tk.type == Token_Type::IDENTIFIER) {
@@ -160,10 +145,34 @@ parse_leaf(Parser &parser) {
 }
 
 static Ast_Node *
+parse_increasing_precedence(Parser &parser, Ast_Node *left, s32 min_prec) {
+    Ast_Node *result;
+
+    Token next = *parser.next;
+
+    if (is_binary_operator(next)) {
+        s32 next_prec = get_precedence(next);
+
+        if (next_prec <= min_prec) {
+            result = left;
+        } else {
+            ++parser.next;
+            Ast_Node *right = parse_expression(parser, next_prec);
+            result = make_binary(left, to_operator(next), right);
+        }
+    } else {
+        ASSERT(0);
+    }
+
+    return result;
+}
+
+static Ast_Node *
 parse_expression(Parser &parser, s32 min_prec) {
     Ast_Node *left = parse_leaf(parser);
 
     for (;;) {
+        if (parser.next->type == Token_Type::SEMICOLON) { break; } // @TODO: is it a right place?
         Ast_Node *node = parse_increasing_precedence(parser, left, min_prec);
         if (node == left) { break; }
         left = node;
@@ -172,17 +181,24 @@ parse_expression(Parser &parser, s32 min_prec) {
     return left;
 }
 
-static void
+static Ast_Node *
 parse_statement(Parser &parser) 
 {
+    Ast_Node *result = new Ast_Node{};
+    result->type = AST_STATEMENT;
+
     switch (parser.next->type) {
+        case Token_Type::SEMICOLON: {
+            ++parser.next;
+        } break;
+
         case Token_Type::IDENTIFIER: {
-            Ast_Node *DEBUG_node = parse_expression(parser, DEBUG_min_prec);
+            ASSERT(0);
         } break;
 
         case Token_Type::NUMBER: {
             Ast_Node *DEBUG_node = parse_expression(parser, DEBUG_min_prec);
-            printf("C++ piece of shit.\n");
+            ++parser.next;
         } break;
 
         case Token_Type::KEYWORD: {
@@ -206,38 +222,37 @@ parse_statement(Parser &parser)
 
         INVALID_DEFAULT_CASE;
     }
+
+    return result;
 }
 
-static void
+static std::vector<Ast_Node *>
 parse(Token_List *token_list) {
+    std::vector<Ast_Node *> statements;
     Parser parser(token_list);
-    parse_statement(parser);
+    while (parser.next->type != Token_Type::END_OF_FILE) {
+        statements.push_back(parse_statement(parser));
+    }
+    return statements;
 }
 
-int main(void)
-{
+int main(void) {
     const char *file_name = "main.ember";
     Buffer code = read_entire_file_and_null_terminate(file_name);
-    if (code.size)
-    {
+    if (code.size) {
         Token_List token_list = {};
         token_list.init(&token_list, MB(1));
 
-        if (token_list.base)
-        {
+        if (token_list.base) {
             tokenize(code, &token_list);
 #if 1
             DEBUG_print_tokens(&token_list);
 #endif
-            parse(&token_list);
-        }
-        else
-        {
+            std::vector<Ast_Node *> statements = parse(&token_list);
+        } else {
             fprintf(stderr, "ERROR: Failed to malloc token list.\n");
         }
-    }
-    else
-    {
+    } else {
         fprintf(stderr, "ERROR: Buffer's size is 0. (%s).\n", file_name);
     }
 
